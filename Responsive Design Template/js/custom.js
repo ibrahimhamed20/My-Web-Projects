2b6d05b3c177b7020c06f1b03ea71545c0e17877
Ibrahim Hamed <39784258+ibrahimhamed20@users.noreply.github.com>
2018-08-31 20:02:39 +0200

    Add files via upload

---
+++ Responsive Design Template/js/custom.js
@@ -0,0 +1,9 @@
+/*global $ */
+$(function(){
+    'use strict';
+   $('.info-list li').click(function() {
+       $(this).addClass('selected').siblings('li').removeClass('selected');
+       $('.info-content div').hide();
+       $('.' + $(this).data('class')).fadeIn();
+   });
+});
